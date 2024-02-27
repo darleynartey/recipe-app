a117b16afaa133adab063a022b37c4790eb9c5df
darleynartey <barbaradnartey@gmail.com>
2024-02-27 16:01:00 +0000

    Getting searching recipes to work

--- src/Pages/recipes/index.jsx
+++ src/Pages/recipes/index.jsx
@@ -1,13 +1,16 @@
 import { Card, CardContent, CardMedia, Container, Grid, TextField, Typography, CardActionArea } from "@mui/material";
 import { useEffect, useState } from "react";
+import undraw_empty from '../../assets/images/undraw_empty.svg'
 
 export default function Recipes() {
     const [recipes, setRecipes] = useState([]);
+    const [keyword, setKeyword] = useState("");
 
     const getRecipes = () => {
         //prepare url
         const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
-        url.searchParams.append('apiKey', 'bd9cdea36ce848398b266f4886b7e5f4');
+        url.searchParams.append('apiKey', process.env. REACT_APP_SPOONACULAR_API_KEY);
+        url.searchParams.append('query', keyword);
         //fetch recipes from the API
         fetch(url)
             .then(response => response.json())
@@ -21,18 +24,19 @@ export default function Recipes() {
             })
 
     }
-    useEffect(getRecipes, []);
+    useEffect(getRecipes, [keyword]);
     return (
         <Container sx={{ my: '2rem' }} >
             <TextField
                 fullWidth id="outlined-basic"
                 label="Enter a keyword to search recipes and hit Enter"
-                variant="outlined" />
-
-
+                variant="outlined" 
+                onKeyDown={event => event.key === 'Enter' &&
+                setKeyword(event.target.value)} /> 
+                
 
             <Grid sx={{ mt: '1rem' }} container spacing={3}>
-                {recipes.map(recipe => (<Grid key={recipe.id} item xs={4}>
+                {recipes.length > 0 ? recipes.map(recipe => (<Grid key={recipe.id} item xs={4}>
                     <Card sx={{ maxWidth: 345, height: '100%' }}>
                         <CardActionArea sx={{ height: '100%' }}>
                             <CardMedia
@@ -48,7 +52,7 @@ export default function Recipes() {
                             </CardContent>
                         </CardActionArea>
                     </Card>
-                </Grid>))}
+                </Grid>)) : <img src={undraw_empty} width="50"/>}
             </Grid>
         </Container>
     );

9fce3280550828e4bd8b4cf5b2223ab34f71ec99
darleynartey <barbaradnartey@gmail.com>
2024-02-29 14:43:24 +0000

    Change made to App.js

--- src/App.js
+++ src/App.js
@@ -4,21 +4,19 @@ import Recipe from "./Pages/recipe";
 import Recipes from "./Pages/recipes";
 
 const router = createBrowserRouter([
-  {path:"/", element: <Recipes />},
-  {path: "/recipes", element: <Recipes />},
-  {path: "/recipes/:id", element: <Recipe />}
+  { path: "/", element: <Recipes /> },
+  { path: "/recipes", element: <Recipes /> },
+  { path: "/recipes/:id", element: <Recipe /> }
 
 ]);
 
 function App() {
   return (
     <>
-    <Navbar />
-    <RouterProvider router={router} />
-    <Recipes /> 
-    <Recipe />  
+      <Navbar />
+      <RouterProvider router={router} />
     </>
-      
+
   );
 }
 
